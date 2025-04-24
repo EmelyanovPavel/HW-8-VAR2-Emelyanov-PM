9cb5d186eef78dca351713594ea9ab452a3a80b7
EmelyanovPavel <gdetoty@list.ru>
2025-04-24 12:22:48 +0300

    Update Recursive functions.cpp
    Изменён код для решения второго варианта(примера) из задания 2.

--- Recursive functions.cpp
+++ Recursive functions.cpp
@@ -24,36 +24,23 @@ void task1()
 // Write a recursive function to find the nth term and the sum of the first 
 // n terms of the progression
 
-void Progres(int n, float q, float b1, int i)
+void rekurs_calc(int nmax, int n, float q, float& b, float& s) 
 {
-    float b, s;
-    if (i == 1)
-    {
-        b = b1;
-        s = b1;
-    }
-    else if (i > 1)
-    {
-        b = b1 * pow(q, i - 1);
-        s += b;
 
+    if (n) {
+        b = b * q;
+        s = s + b;
+        std::cout << "n = " << nmax - n << " b = " << b << " q = " << q << " sum = " << s << std::endl;
     }
-    i--;
-    if (i == 0) {
-        std::cout << "b=" << b << "\ts=" << s << std::endl;
-    }
-    Progres(i, q, b1, i);
+
 }
-void task2()
+
+void task2() 
 {
-    int n; float q, b1;
-    std::cout << "b1=";
-    std::cin >> b1;
-    std::cout << "q=";
-    std::cin >> q;
-    std::cout << "n=";
-    std::cin >> n;
-    Progres(n, q, b1, n);
+    int nmax, n;
+    float b, q, s;
+    std::cin >> nmax >> n >> b >> q >> s;
+    rekurs_calc(nmax, n, q, b, s);
 }
 
 int main()
